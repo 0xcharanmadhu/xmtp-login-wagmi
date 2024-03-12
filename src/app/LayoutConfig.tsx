5a92e136053d9d3942578c03c5b23494469276cd
fabri <fguespe@gmail.com>
2024-03-12 08:48:32 -0300

    init

--- src/app/LayoutConfig.tsx
+++ src/app/LayoutConfig.tsx
@@ -1,42 +1,60 @@
-'use client'
+"use client";
 
-import { WagmiProvider, createConfig } from 'wagmi';
+import { WagmiProvider, createConfig } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import {mainnet,polygon,optimism,bsc,arbitrum,avalanche,celo,polygonMumbai} from 'viem/chains';
-import { ConnectKitProvider, getDefaultConfig } from 'connectkit';
-import { XMTPProvider } from '@xmtp/react-sdk';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  bsc,
+  arbitrum,
+  avalanche,
+  celo,
+  polygonMumbai,
+} from "viem/chains";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { XMTPProvider } from "@xmtp/react-sdk";
 
 const config = createConfig(
-	getDefaultConfig({
-		appName: "tempApp",
-        chains:[mainnet,polygon,optimism,bsc,arbitrum,avalanche,celo,polygonMumbai],
-		walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-        appDescription: "tempApp",
-        appUrl: "https://tempApp.io",
-	})
-)
+  getDefaultConfig({
+    appName: "tempApp",
+    chains: [
+      mainnet,
+      polygon,
+      optimism,
+      bsc,
+      arbitrum,
+      avalanche,
+      celo,
+      polygonMumbai,
+    ],
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+    appDescription: "tempApp",
+    appUrl: "https://tempApp.io",
+  })
+);
 
 type LayoutConfigProp = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 const queryClient = new QueryClient();
 
-export default function LayoutConfig({ children}: LayoutConfigProp){
-	
-    return(<>
-			<WagmiProvider config={config}>
-                <QueryClientProvider client={queryClient}>
-						<ConnectKitProvider theme="midnight" 
-											customTheme={{
-												"--ck-font-family": '"Quicksand", sans-serif',
-											}}
-						>	
-								<XMTPProvider>
-									{children}
-								</XMTPProvider>
-						</ConnectKitProvider>
-                </QueryClientProvider>
-			</WagmiProvider>
-    	</>)
+export default function LayoutConfig({ children }: LayoutConfigProp) {
+  return (
+    <>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <ConnectKitProvider
+            theme="midnight"
+            customTheme={{
+              "--ck-font-family": '"Quicksand", sans-serif',
+            }}
+          >
+            <XMTPProvider>{children}</XMTPProvider>
+          </ConnectKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </>
+  );
 }
